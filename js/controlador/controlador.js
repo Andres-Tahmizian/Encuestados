46844370c3efdac6b32e4aeb28234590e69efbaa
Andres Tahmizian <andrestahmizian@gmail.com>
2020-09-20 22:11:43 -0300

    Actualizado

---
+++ js/controlador/controlador.js
@@ -0,0 +1,28 @@
+// Controlador//
+
+var Controlador = function(modelo) {
+  this.modelo = modelo;
+};
+
+Controlador.prototype = {
+  agregarPregunta: function(pregunta, respuestas) {
+      this.modelo.agregarPregunta(pregunta, respuestas);
+  },
+
+  borrarPregunta: function(idPregunta) {
+      this.modelo.borrarPregunta(idPregunta);
+  },
+
+  editarPregunta: function (idPregunta, textoNuevaPregunta) {
+      this.modelo.editarPregunta(idPregunta, textoNuevaPregunta);
+  },
+
+  borrarTodo: function (){
+    this.modelo.borrarTodo();
+  },
+
+  agregarVoto: function (nombrePregunta, respuestaSeleccionada) {
+    console.log('la siguiente información llegó al controlador', nombrePregunta, respuestaSeleccionada)
+      this.modelo.agregarVoto (nombrePregunta, respuestaSeleccionada);
+  }
+}
---
+++ js/modelo/modelo.js
@@ -0,0 +1,122 @@
+//Modelo//
+
+var Modelo = function() {
+  this.preguntas = [];
+  this.ultimoId = 0;
+
+  //inicializacion de eventos
+  this.preguntaAgregada = new Evento(this);
+  this.preguntaBorrada = new Evento(this);
+  this.preguntasBorradas = new Evento(this);
+  this.preguntaEditada = new Evento (this);
+  this.respuestaVotada = new Evento (this);
+
+
+
+  this.traerPreguntasGuardadas();
+};
+
+Modelo.prototype = {
+  //se obtiene el id más grande asignado a una pregunta
+  obtenerUltimoId: function() {
+    let data = this.preguntas.length;
+    if (data == 0){
+      return -1;
+    } else {
+      for(let i=0; i<this.preguntas.length;i++){
+      data = this.preguntas[i].id;
+    }
+  }
+  return data;
+  },
+
+  traerPreguntasGuardadas: function () {
+    let preguntasAnteriores = localStorage.getItem('preguntas')
+    if (preguntasAnteriores) {
+      this.preguntas = JSON.parse(localStorage.getItem('preguntas'));
+    } else {
+      localStorage.setItem('preguntas','[]');
+    }
+  },
+
+  //Se actualiza el array de preguntas.
+  guardar: function() {
+    this.preguntas = JSON.parse(localStorage.getItem('preguntas'));
+  },
+
+  //Se actualiza el storage cuando se suma una nueva pregunta
+  agregarAlStorage: function (nuevaPregunta){
+    let preguntasParseadas = JSON.parse(localStorage.getItem('preguntas'));
+    preguntasParseadas.push(nuevaPregunta);
+    localStorage.setItem('preguntas',JSON.stringify(preguntasParseadas));
+  },
+
+  borrarUnaDelStorage: function (id) {
+    let preguntasParseadas = JSON.parse(localStorage.getItem('preguntas'));
+    let preguntaABorrar = preguntasParseadas.find((pregunta) => pregunta.id==id);
+    let preguntasActualizadas = preguntasParseadas.filter ((pregunta)=> pregunta != preguntaABorrar);
+    localStorage.setItem('preguntas', JSON.stringify(preguntasActualizadas));
+
+  },
+
+  borrarTodasDelStorage: function () {
+    localStorage.setItem('preguntas', '[]');
+  },
+
+  editarDelStorage: function (textoNuevaPregunta, idPreguntaVieja) {
+    let preguntasParseadas = JSON.parse(localStorage.getItem('preguntas'));
+    let preguntaAActualizar = preguntasParseadas.find((pregunta) => pregunta.id==idPreguntaVieja);
+    preguntaAActualizar.textoPregunta = textoNuevaPregunta;
+    localStorage.setItem('preguntas', JSON.stringify(preguntasParseadas));
+  },
+
+  //Revisar esta función: puedo acceder a los votos de la respuesta, pero no se guarda en el LS.
+  votarStorage: function (nombrePregunta, respuestaSeleccionada){
+    let preguntasParseadas = JSON.parse(localStorage.getItem('preguntas'));
+    let preguntaAActualizar = preguntasParseadas.find((pregunta) => pregunta.textoPregunta==nombrePregunta);
+    preguntaAActualizar.cantidadPorRespuesta.find((respuesta) => respuesta.textoRespuesta == respuestaSeleccionada).cantidad++
+    localStorage.setItem('preguntas', JSON.stringify(preguntasParseadas));
+  },
+
+
+  //se agrega una pregunta dado un nombre y sus respuestas
+  agregarPregunta: function(textoPreguntaAgregada, respuestasPreguntaAgregada) {
+    var id = this.obtenerUltimoId();
+    id++;
+    var nuevaPregunta = {'textoPregunta': textoPreguntaAgregada, 'id': id, 'cantidadPorRespuesta': respuestasPreguntaAgregada};
+    this.agregarAlStorage(nuevaPregunta);
+    this.guardar();
+    this.preguntaAgregada.notificar();
+  },
+
+  //se borra una pregunta según su ID
+  borrarPregunta: function (idPreguntaBorrada) {
+    this.borrarUnaDelStorage(idPreguntaBorrada);
+    this.guardar();
+    this.preguntaBorrada.notificar();
+  },
+//Borra todo
+  borrarTodo: function () {
+    this.borrarTodasDelStorage();
+    this.guardar();
+    this.preguntasBorradas.notificar();
+  },
+
+//Edita una pregunta segun su id
+  editarPregunta: function (idPreguntaEditada, textoNuevaPregunta) {
+    this.editarDelStorage(textoNuevaPregunta, idPreguntaEditada);
+    this.guardar();
+    this.preguntaEditada.notificar();
+  },
+// agrega votos segun el nombre y respuestas seleccionadas
+  agregarVoto: function (nombrePregunta, respuestaSeleccionada){
+    this.votarStorage (nombrePregunta, respuestaSeleccionada);
+    this.guardar();
+    this.respuestaVotada.notificar();
+  },
+}
+
+
+
+
+
